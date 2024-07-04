3edeb6fbe1eb1d1d2e0cfeac768cd51c14e50fad
Adrián Pereira Guerra <adrianpg@empathy.co>
2024-07-04 15:03:01 +0200

    feat: injector

--- src/injector.ts
+++ src/injector.ts
@@ -1,46 +1,68 @@
+import { Injectable } from "./decorators/injectable";
 
 /**
-* CRUD Service for CRUD operations against BE / DB
-*/
+ * CRUD Service for CRUD operations against BE / DB
+ */
 class CrudService {
-    getData(entity: string) {
-      return `Some Data from -> ${entity}`;
+  getData(entity: string) {
+    return `Some Data from -> ${entity}`;
+  }
+}
+
+/**
+ * Service to retrieve/crate/update comments
+ */
+@Injectable()
+class CommentsService {
+  constructor(public crudService: CrudService) {}
+
+  getComments() {
+    return this.crudService.getData("/comments");
+  }
+}
+
+/**
+ * Service to retrieve/crate/update comments
+ */
+@Injectable()
+class MoviesService {
+  constructor(
+    private commentsService: CommentsService,
+    private crudService: CrudService
+  ) {}
+  getMovies() {
+    return this.crudService.getData("/movies");
+  }
+  getComments() {
+    return this.commentsService.getComments();
+  }
+}
+
+const movies = new MoviesService(
+  new CommentsService(new CrudService()),
+  new CrudService()
+);
+
+console.log(movies.getComments());
+console.log(movies.getMovies());
+
+const Injector = new (class {
+  instMap: Record<string, any> = {};
+  resolve(target: any) {
+    let tokens = Reflect.getMetadata("design:paramtypes", target);
+    console.log(tokens);
+    let injections = [];
+    if (tokens) {
+      injections = tokens.map((token: any) => Injector.resolve(token));
     }
-   }
-    
-   /**
-   * Service to retrieve/crate/update comments
-   */
-   class CommentsService {
-    constructor(public crudService: CrudService) {}
-    
-    getComments() {
-      return this.crudService.getData('/comments');
+
+    if (this.instMap.hasOwnProperty(target.name)) {
+      return this.instMap[target.name];
+    } else {
+      this.instMap[target.name] = new target(...injections);
+      return this.instMap[target.name];
     }
-   }
-    
-   /**
-   * Service to retrieve/crate/update comments
-   */
-   class MoviesService {
-    constructor(
-      private commentsService: CommentsService,
-      private crudService: CrudService
-    ) {}
-    getMovies() {
-      return this.crudService.getData('/movies');
-    }
-    getComments() {
-      return this.commentsService.getComments();
-    }
-   }
+  }
+})();
 
-
-
-
-   const movies = new MoviesService(
-    new CommentsService(new CrudService()),
-    new CrudService()
-   );
-
-   
+console.log(Injector.resolve(MoviesService));
